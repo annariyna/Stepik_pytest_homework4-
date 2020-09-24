404045a121adbb71e24263283df0000d07ecfd8b
annariyna <70909249+annariyna@users.noreply.github.com>
2020-09-24 10:15:46 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption ('-language', action = 'store', default = None, help = "Choose GUI language for tests")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption ("language")
+    if language is None: raise pytest.UsageError("test run should contain language for test")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+
+    print("\nopen browser for test..")
+    browser = webdriver.Chrome(options=options)
+
+    browser.implicitly_wait(5)
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
